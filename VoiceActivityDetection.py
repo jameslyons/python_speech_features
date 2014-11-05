ff5dff28cc8a95484faed37c9fccc6f96656e05b
varunnayyar <nayyarv@gmail.com>
2014-11-05 15:45:32 +1100

    Some reformatting

--- VoiceActivityDetection.py
+++ VoiceActivityDetection.py
@@ -6,11 +6,7 @@ __doc__ = \
 
     These functions can be used in most base functions by passing VAD = myVADfunction where
     myVADfunction follows the template provided.
-
-
     """
-
-import numpy as np
 
 
 def templateVAD(frames, sig):
@@ -32,10 +28,8 @@ def simpleVAD(frames, sig, threshold=0.01):
     Note that the variance of frame/signal represents the average power of the frame/signal
     so this is a power threshold activity detector applied along the frames
     """
+    import numpy as np
+
     frameVars = np.var(frames, 1)
     reducedFrames = frames[np.where(frameVars > sig.var() * threshold)]
     return reducedFrames
-
-
-if __name__ == "__main__":
-    pass
