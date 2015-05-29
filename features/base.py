7680ae3d725395e8ef14be16d113684c9446ea85
Pablo Jadzinsky <jadz@stanford.edu>
2015-05-29 15:22:14 -0700

    Associated xrange to range to make it work in python3

--- features/base.py
+++ features/base.py
@@ -3,7 +3,13 @@
 import numpy
 from features import sigproc
 from scipy.fftpack import dct
-    
+
+# make it python3.x compatible
+try:
+  xrange(1)
+except:
+  xrange=range
+
 def mfcc(signal,samplerate=16000,winlen=0.025,winstep=0.01,numcep=13,
           nfilt=26,nfft=512,lowfreq=0,highfreq=None,preemph=0.97,ceplifter=22,appendEnergy=True):
     """Compute MFCC features from an audio signal.
