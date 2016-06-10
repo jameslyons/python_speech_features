561daff1eb8f3b311fb5c4fe43bd8db676ae7b78
ybdarrenwang <dwang@fortemedia.com>
2016-06-10 16:17:51 -0700

    implemented delta function in base.py for delta feature calculation

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from features import mfcc
+from features import delta
 from features import logfbank
 import scipy.io.wavfile as wav
 
 (rate,sig) = wav.read("file.wav")
 mfcc_feat = mfcc(sig,rate)
+d_mfcc_feat = delta(mfcc_feat, 2)
 fbank_feat = logfbank(sig,rate)
 
 print(fbank_feat[1:3,:])
