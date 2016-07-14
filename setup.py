b0c2c52b49d2bd7c81cc0a522cb8ae0fcb20d066
James Lyons <james.lyons0@gmail.com>
2016-07-14 14:00:57 +1000

    updated some fields for pypi release

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
-setup(name='speech-features',
+setup(name='python_speech_features',
       version='0.1',
       description='Python Speech Feature extraction',
       author='James Lyons',
+      author_email='james.lyons0@gmail.com',
+      license='MIT',
       url='https://github.com/jameslyons/python_speech_features',
       packages=['features'],
     )
